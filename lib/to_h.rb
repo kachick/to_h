a035269954784734ee624876291507be61ce71af
Matthew Kerwin <matthew@kerwin.net.au>
2014-03-30 21:38:08 +1000

    keyword -> const

--- lib/to_h.rb
+++ lib/to_h.rb
@@ -44,7 +44,7 @@ if RUBY_VERSION < '2.0'
     # @return [Hash]
     def to_h
       hsh = {}
-      none = class.new.new
+      none = Class.new.new
       each do |k,v=none,*x|
         raise ArgumentError, "element has wrong array length (expected 2, was 1)" if v == none
         raise ArgumentError, "element has wrong array length (expected 2, was #{x.length+2})" if x.any?
